2d83cde203ae4bee8af97c436f9ba2ff771d0493
橋口　巧 <takutaku0320@gmal.com>
2018-06-22 15:12:08 +0900

    結果画面 途中経過

--- pages/classes/uploads/results/result.vue
+++ pages/classes/uploads/results/result.vue
@@ -22,116 +22,34 @@ div.result-container
 </template>
 
 <script>
-import { mapActions,mapGetters } from 'vuex'
+import { mapActions, mapGetters, mapMutations} from 'vuex'
 export default {
   data() {
     return {
       centerDialogVisible: false,
+      historyId: null,
       tableData: ['川田大秀','松永拓也','橋口巧','佐竹直人']
     }
-   },
-   created () {
-    this.historyId = this.$route.params.historyId
-   },
-   methods: {
+  },
+  created () {
+  },
+
+  computed: {
+    ...mapActions('result', ['find']),
+    ...mapGetters('resut', ['getResult','getHistory'] )
+
+  },
+
+  methods: {
+
+
     handleDelete(row) {
       alert(row);
     }
-//- ...mapActions('result', ['find']),
-//- ...mapGetters('getters', ['getResult'])
-//-
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.result-container{
-  padding-left: 60px;
-  padding-right: 60px;
-  margin-left: 80px;
-  margin-right: 80px;
-}
 
-.result-list{
-  margin-top: 30px;
-  margin-bottom: 30px;
-}
-
-.el-table{
-  font-size: 17px;
-}
-
-.button-container{
-  text-align: right;
-}
-
-.el-button{
-  width: 70px;
-  font-size: 16px;
-}
-
-.dialog-comment{
-  font-size: 18px;
-  text-align: center;
-  padding: 10px;
-  margin-bottom: 50px;
-}
-
-.dialog-button{
-  text-align: center;
-  margin-bottom: 30px;
-}
-
-.dialog-back{
-  margin-left: 60px;
-  width: 80px;
-  font-size: 16px;
-}
-
-.dialog-delete{
-  margin-right: 60px;
-  width: 80px;
-  font-size: 16px;
-}
-//スマホ対応css
-@media (max-width: 599px) {
-  .result-container{
-    padding-left: 4px;
-    padding-right: 4px;
-    margin-left: 5px;
-    margin-right: 5px;
-  }
-
-  .result-list{
-    margin-top: 5px;
-    margin-bottom: 20px;
-  }
-
-  .el-table{
-    font-size: 13px;
-  }
-
-  .el-button{
-    margin-left: 27px;
-    width: 53px;
-    font-size: 11px;
-  }
-
-  .dialog-comment{
-    font-size: 10px;
-    text-align: center;
-    padding: 16px;
-    margin-bottom: 50px;
-  }
-
-  .dialog-back{
-    width: 50px;
-    font-size: 12px;
-  }
-
-  .dialog-delete{
-    width: 50px;
-    font-size: 12px;
-  }
-}
 </style>
--- store/result.js
+++ store/result.js
@@ -1,25 +1,33 @@
-//
 export const state = () => ({
-  result: []
+  result: [],
+  historyId: 1
 })
 
 export const mutations = () => ({
   setResult (state, result) {
     state.result = result
+  },
+
+  setHistory (state, historyId) {
+    state.historyId = historyId
   }
 })
 
 export const getters = () => ({
   getResult(state){
     return state.result
+  },
+
+  getHistory(state){
+    return state.historyId
   }
 })
 
 export const actions = () => ({
-  async find ({dispatch, commit}, { params = null }) {
+  async find ({dispatch, commit}, { historyId }) {
     try {
-      const result = await dispatch('api/get', { url: '/api/v1/history_students/attendance_result?history_id=${historyId}', params: params }, { root: true })
-      commit('setResult', result)
+      const result = await dispatch('api/get', {url: `/history_students/attendance_result?history_id=${historyId}`, params: []}, {root: true})
+      commit('setHistory', result)
       return result
     } catch (e) {
       throw e
