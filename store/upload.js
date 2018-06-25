f8fb6b1424f73d2ac13be5f6c1f05c777c3957d8
橋口　巧 <takutaku0320@gmal.com>
2018-06-25 09:48:13 +0900

    historyId適用

--- store/upload.js
+++ store/upload.js
@@ -1,6 +1,7 @@
 export const state = () => ({
   uploadImage: null,
   subjectId: null,
+  historyId:null
 })
 
 export const mutations = {
@@ -11,18 +12,22 @@ export const mutations = {
   setSubject (state, subjectId) {
     state.subjectId = subjectId
   },
+  setHistory (state, historyId) {
+    state.historyId = historyId
+  }
 }
 
 export const actions = {
-  async postImage ({dispatch, state}) {
+  async postImage ({dispatch, state, commit}) {
     try {
       let params = new FormData()
       params.append('image', state.uploadImage)
       params.append('subject_id', state.subjectId)
       console.log(params.get('image'))
       console.log(params.get('subject_id'))
-
       const result = await dispatch('api/post', {url: '/histories/attendance_image_upload', params: params}, {root: true})
+      console.log(result.id)
+      commit('setHistory', result.id)
     } catch (e) {
        throw e
     }
