a84bed9633e6972b49a9a459ee89d7798d90a92e
橋口　巧 <takutaku0320@gmal.com>
2018-07-20 14:27:56 +0900

    クラス名簿モーダル処理追加

---
+++ components/dialog/classRoster.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+el-dialog(title='クラス名簿', :visible.sync='outerVisible', :close-on-click-modal= 'false', :show-close='false')
+  el-table(:data='tableData')
+    el-table-column(label='No', width='80%')
+      template(v-for="(row,index) in tableData", slot-scope='scope')
+        span {{ scope.$index + 1 }}
+    el-table-column(label='名前')
+      template(v-for="(row,index) in tableData", slot-scope='scope')
+        span {{ scope.row }}
+    el-table-column
+      template(v-for="(row,index) in tableData", slot-scope='scope')
+        div.button-container
+          el-button.absence-button(size='mini', type='primary', @click='atendanceClasses(scope.$index, scope.row)') 出席
+  .dialog-footer(slot='footer')
+    el-button(@click='toggleClassRosterModal') 閉じる
+</template>
+<script>
+export default {
+  props: {
+    tableData: { type: Array, required: true },
+    outerVisible: { type: Boolean, required: true },
+    toggleClassRosterModal: { type: Function, required: true },
+    atendanceClasses: { type: Function, required: true }
+
+  },
+  data: () => ({
+  }),
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
